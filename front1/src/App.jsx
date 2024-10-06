578acbfc8cebaa678998a0e6c44664163c76f74b
Sergi Mias Martinez <sergimima@gmail.com>
2024-10-06 21:18:01 +0200

    Build_v1

--- front1/src/App.jsx
+++ front1/src/App.jsx
@@ -1,24 +1,65 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { ethers } from 'ethers'
+import { ethers, JsonRpcProvider } from 'ethers'
 
-const provider = new ethers.BrowserProvider(window.ethereum);
-const ethereum = (window).ethereum;
+const sc = "0x90D3d13Eda8F15F0a0F0093f4Ca8Aa4846E8d119"
+let httpProvider = new JsonRpcProvider('http://localhost:8545')
+const abi = `[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"contador","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"dec","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"inc","outputs":[],"stateMutability":"nonpayable","type":"function"}]`;
+
+const ethereum = window.ethereum;
+
 function App() {
   const [address, setAddress] = useState(null);
-  
-  useEffect(() =>{
-    const accounts = ethereum.request({method: 'eth_requestAccounts',}).then(addresses => {setAddress(addresses[0])
-      ethereum.on("accountsChanged",(addresses => {
-        setAddress(addresses[0])
-      }))
+  const [contador, setContador] = useState(null);
+  const [tx, setTx] = useState(null);
+
+  useEffect(() => {
+    const contract = new ethers.Contract(sc, abi, httpProvider);
+    contract.contador().then(r => {setContador(r.toString());
     })
+  }, [address])
+  
+  useEffect(() => {
+    if (ethereum) {
+      ethereum.request({method: 'eth_requestAccounts'}).then(addresses => {
+        setAddress(addresses[0])
+        ethereum.on("accountsChanged", (addresses => {
+          setAddress(addresses[0])
+        }))
+      })
+    } else {
+      console.log("Please install MetaMask!")
+    }
   }, [])
+
+  async function op(valor) {
+    if (!ethereum) {
+      console.log("Please install MetaMask!")
+      return
+    }
+    
+    console.log(sc);
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+    
+    const contract = new ethers.Contract(sc, abi, signer);
+    const tx  = valor == 1 ? await contract.inc(): await contract.dec()
+    const receipt = await tx.wait();
+    setTx(JSON.stringify({tx, receipt}, null, 4))
+    const c = await contract.contador()
+    console.log("nuevo contador ", c)
+    setContador(c.toString())
+  }
+
   return (
     <>
       <div>
-        <p>La cuenta es: {address}</p>
-        </div>
+        <h3>La cuenta es: {address}</h3>
+        <p>Contador: {contador}</p>
+        <pre>{tx}</pre>
+        <button onClick={() => op(1)}>Incrementar</button>
+        <button onClick={() => op(-1)}>Decrementar</button>
+      </div>
     </>
   )
 }
---
+++ sc/execContract.js
@@ -0,0 +1,40 @@
+const {ethers, Contract, JsonRpcProvider, Wallet} = require('ethers')
+const httpProvider = new JsonRpcProvider('http://localhost:8545')
+const fs = require('fs')
+const abi = JSON.parse(fs.readFileSync('./out/contador_sol_Contador.abi').toString());
+
+const walletJson = `{
+    "address": "96610b94e73c1504a212dc8af66a05a6a6553a4c",
+    "crypto": {
+        "cipher": "aes-128-ctr",
+        "ciphertext": "75c9edc899f05cae0f1e0cca1e17bb50fc05ce74140c6c3527fb4e9c0aaaa157",
+        "cipherparams": {
+            "iv": "3d6f86e7c163d96a36592413c6b96365"
+        },
+        "kdf": "scrypt",
+        "kdfparams": {
+            "dklen": 32,
+            "n": 262144,
+            "p": 1,
+            "r": 8,
+            "salt": "39446b165b380ee762e4cc900f127affdbd9d5824fb91a087eb13a3e9a12d9f7"
+        },
+        "mac": "54b2d77454ef1116a6efd14979c35f28fcc8d6133e6517bbebf749b15f451695"
+    },
+    "id": "9e3ae26e-736a-48d6-bfe1-27ba574745e7",
+    "version": 3
+}`
+
+async function main() {
+    let wallet = await Wallet.fromEncryptedJsonSync(walletJson, "")
+    wallet = await wallet.connect(httpProvider);
+
+    const c = fs.readFileSync("contractAddress.txt").toString()
+    const contract = new Contract(c, abi, wallet)
+    const result = await contract.contador()
+
+    const tx = await contract.inc()
+    console.log(tx)
+}
+
+main()
